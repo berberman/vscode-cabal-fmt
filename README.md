b8fa371b179280ddd53210eb93faa4fdfe391e7a
Potato Hatsue <1793913507@qq.com>
2021-02-06 18:03:50 +0800

    Make this extension available

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vscode-cabal-fmt
+
+This vscode plugin provides a document formatter for .cabal files, using [cabal-fmt](https://github.com/phadej/cabal-fmt).
+You need install `cabal-fmt` manually before using this extension.
--- src/extension.ts
+++ src/extension.ts
@@ -11,21 +11,20 @@ const exeName = "cabal-fmt";
 
 export function activate(context: vscode.ExtensionContext) {
 
-	vscode.languages.registerDocumentFormattingEditProvider({ pattern: "**​/*.cabal" }, new CabalFormatProvider());
+	vscode.languages.registerDocumentFormattingEditProvider('cabal', new CabalFormatProvider());
+
 }
-
-export function deactivate() { }
-
 
 class CabalFormatProvider implements vscode.DocumentFormattingEditProvider {
 	protected findManual(): string | null {
-		let path = vscode.workspace.getConfiguration('vscode-cabal-fmt').binaryPath;
+		let path = vscode.workspace.getConfiguration('cabal-fmt').binaryPath;
 		if (path === '') {
 			return null;
 		}
 		path = path.replace('${HOME}', os.homedir).replace('${home}', os.homedir).replace(/^~/, os.homedir);
 
 		if (!commandExists(path)) {
+			vscode.window.showErrorMessage(`Path to cabal-fmt is set to an unknown place: ${path}`);
 			throw new Error(`Unable to find ${path}`);
 		}
 		return path;
@@ -40,16 +39,16 @@ class CabalFormatProvider implements vscode.DocumentFormattingEditProvider {
 	provideDocumentFormattingEdits(document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
 
 		return new Promise((resolve, rejects) => {
-			let filepath = document.uri.toString();
-			const binaryPath = this.findLocal() ?? this.findManual();
+
+			let filepath = document.fileName;
+			const binaryPath = this.findManual() ?? this.findLocal();
 			if (binaryPath === null) {
+				vscode.window.showErrorMessage(`Path to cabal-fmt is null`);
 				rejects("Unable to call cabal-fmt");
 			}
-			if (filepath.startsWith('file://')) {
-				filepath = path.dirname(document.fileName);
-			}
-			vscode.window.showInformationMessage(`filepath: ${filepath}`);
-			const cmd = child_process.spawn(binaryPath!, [filepath]);
+			const indent = vscode.workspace.getConfiguration('cabal-fmt').indent;
+			vscode.window.showInformationMessage(`Formatting ${path.basename(filepath)}`);
+			const cmd = child_process.spawn(binaryPath!, ["--indent", indent, filepath]);
 			const result: Buffer[] = [];
 			const err: Buffer[] = [];
 			cmd.stdout.on('data', data => {
@@ -65,8 +64,13 @@ class CabalFormatProvider implements vscode.DocumentFormattingEditProvider {
 					const range = document.validateRange(new vscode.Range(0, 0, Infinity, Infinity));
 					resolve([new vscode.TextEdit(range, r)]);
 				} else {
+					vscode.window.showErrorMessage(`Stderr from cabal-fmt: ${e}`);
 					rejects(`error: ${e}`);
 				}
+			});
+			cmd.on('error', e => {
+				vscode.window.showErrorMessage(`Failed to call cabal-fmt: ${e}`);
+				rejects(`error: ${e}`);
 			});
 		});
 	}
